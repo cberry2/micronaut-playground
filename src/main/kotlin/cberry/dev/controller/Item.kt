54af76c1268b4a03c3f53c5736e3495b27d8aeb4
Craig Berry <cberry@chewy.com>
2019-04-10 22:46:17 -0400

    clean

--- src/main/kotlin/cberry/dev/controller/Item.kt
+++
@@ -1,7 +0,0 @@
-package cberry.dev.controller
-
-data class Item(
-    val id: Long = 0L,
-    val title: String = "",
-    val body: String = ""
-)
---
+++ src/main/kotlin/cberry/dev/domain/Item.kt
@@ -0,0 +1,19 @@
+package cberry.dev.entity
+
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "item")
+data class Item(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0L,
+
+    @NotNull
+    @Column(name = "title", nullable = false)
+    val title: String = "",
+
+    @Column(name = "body")
+    val body: String = ""
+)
